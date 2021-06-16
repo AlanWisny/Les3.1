03c90d4dbdd2350543ae34fcfe807f25f4451f9c
Alan <alanwisny@hotmail.com>
2021-06-16 16:21:07 +0200

    Initial commit

---
+++ src/Opdrachten.java
@@ -0,0 +1,99 @@
+
+
+public class Opdrachten {
+
+
+    public static void main(String[] args) {
+
+//       int array[] = {0, -5, -6, 0, 15};
+
+//        8. What does this enhanced for loop do?
+//        kijkt naar elk element, en als het element de waarde 0 heeft dan counter++.
+//        int counter = 0;
+//        for(int x : array){
+//            if (x== 0){
+//                counter++;
+//            }
+//            System.out.println(counter);
+//        }
+//
+
+//        9.Write an enchaned for loop that prints all elements in the array values.
+//        Een manier om door de elements van een array heen te zeven
+//        for(int x : array) {
+//            System.out.println(x);
+//        }
+
+//        met deze for statement krijg je hetzelfde resultaat
+//        for (int i = 0; i < array.length; i++)
+
+//        10. Write an enhanced for loop that multiplies all elements in an array, the sum of the result called product.
+//         int product;
+//        for(int x : array) {
+//            product = x * 2;
+//            System.out.println(product);
+//        }
+
+//6.7. Write enhanced for loops for the following tasks.
+//a. Printing all elements of an array in a single row, separated by spaces.
+//        for(int x : array) {
+//            System.out.print(x + " ");
+//        }
+
+
+//b. Computing the product of all elements in an array.
+//        int product = 0;
+//        for(int x : array) {
+//            product += x;
+//        }
+//        System.out.println(product);
+
+//c. Counting how many elements in an array are negative.
+//        int i = 0;
+//        for(int x : array) {
+//            if(x < 0){
+//                i++;
+//            }
+//        }
+//        System.out.println(i);
+
+//P6.1. Write a code that initializes an array with ten random integers and then prints four lines of output, containing :
+//- Every element at an even index.
+//- Every even element.
+//- All elements in reverse order.
+//- Only the first and last element.
+//     int Array[] = {1, 3, 2, 7, 4, 9, 6, 13, 8, 20};
+//     int i = 0;
+//     int y = Array.length;
+
+//     for(int x : Array){
+//         i++;
+//         if(i % 2 == 0 && i > 0 && i <= 9){
+//             System.out.println();
+//             System.out.print(Array[i] + " ");
+//         } if(x % 2 == 0 && i >= 0 && i <= 9){
+//                 System.out.print(Array[i] + " ");
+//         }
+//     }
+
+//            for (int x : Array) {
+//                i++;
+//                if (y >= 1) {
+//                    y--;
+//                    System.out.print(Array[y] + " ");
+//                    if (y == 1) {
+//                        System.out.println(Array[0]);
+//                    }
+//                    if (y == 9) {
+//                        System.out.println(Array[9]);
+//                    }
+//                }
+//            }
+
+
+
+
+
+
+    }
+}
